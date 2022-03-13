fae3eebb3435323733e018a336632dfad3470217
wan <wan.lucas.90@gmail.com>
2022-03-13 18:38:54 -0300

    versão final

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 const rulesHTML = document.querySelector('.rules');
 const cBoxHTML = document.querySelector('.choise-box');
-const header = document.querySelector('header');
 const playingHTML = document.getElementById('playing');
+
+var pChoise, compCHoise, score = 0; 
 
 document.querySelector('#rules').addEventListener("click", ()=>{
     rulesHTML.style.animation = 'actived 0.6s forwards';
@@ -12,15 +13,25 @@ document.getElementById('rules-close').addEventListener('click', ()=>{
     document.body.style.setProperty('--op', 1);
 });
 
-cBoxHTML.querySelectorAll('button').forEach(bt=>{
+document.querySelectorAll('.choise-box button').forEach(bt=>{
     bt.addEventListener("click", ()=>{
+        playingHTML.style.animation = '';
         setTimeout(()=>{
             cBoxHTML.style.visibility = "hidden";
             playingHTML.style.visibility = "visible";
+            playingHTML.style.animation = 'moveRight 1s forwards';
             updatePlayerChoise(bt.id);
             updateCompChoise();
-        },1000)
+            startAnimations();
+        },300)
     });
+});
+document.getElementById('play-again').addEventListener("click",()=> {
+    cBoxHTML.style.animation = '';
+    cBoxHTML.style.opacity = 0;
+    setTimeout(()=> cBoxHTML.style.animation = 'moveUp 0.5s forwards', 40);
+    playingHTML.style.visibility = "hidden";
+    cBoxHTML.style.visibility = "visible";
 });
 
 function updatePlayerChoise(choise) {
@@ -28,12 +39,39 @@ function updatePlayerChoise(choise) {
     <div>
     <button class="choise-bt" id="${choise}" aria-label="${choise}"><img src="images/icon-${choise}.svg" alt="${choise}"></button>
     </div>`
+    pChoise = choise;
 };
 function updateCompChoise() {
-    const choises = ['paper', 'scissors', 'rock'];
-    const choise = choises[Math.floor(Math.random() * 2)];
-    playingHTML.querySelector('#comp-choise').innerHTML = `
+    const choises = [ 'rock','paper','scissors'];
+    const choise = choises[Math.round(Math.random() * 2)];
+    document.querySelector('#comp-choise').innerHTML = `
     <div>
-    <button class="choise-bt" id="${choise}" aria-label="${choise}"><img src="images/icon-${choise}.svg" alt="${choise}"></button>
+       <button class="choise-bt" id="${choise}" aria-label="${choise}"><img src="images/icon-${choise}.svg" alt="${choise}"></button>
     </div>` 
+    compCHoise = choise;
 }
+function verifyWinner() {
+    let tabel = ['rock','paper','scissors'], res;
+    if(pChoise == compCHoise) return 0;
+    if(tabel.findIndex(e=>e==pChoise) == 0) tabel.push(tabel.shift());
+    res = tabel[tabel.findIndex(e=> e==pChoise) - 1] == compCHoise ? 1 : 2;
+    score += res == 1 ? 1 : -1;
+    return res;
+}
+function updateScore() { 
+     document.getElementById('score').innerText = score;
+}
+function startAnimations() {
+    const btSTL = document.querySelector('#comp-choise button').style, resSTL = document.querySelector('.result').style;
+    const resultHTML = document.getElementById('result'), result = verifyWinner();
+    if(result == 0) resultHTML.innerText = 'DRAW'
+    else resultHTML.innerText = result == 1  ? 'YOU WIN': "YOU LOSE";
+    btSTL.animation = '';
+    resSTL.animation = '';
+    setTimeout(()=> {
+        btSTL.animation = 'flip 1s 1s forwards';
+        resSTL.animation = 'fade 1.5s 2s forwards';
+    },40);
+    setTimeout(()=>updateScore(),2000);
+}
+
