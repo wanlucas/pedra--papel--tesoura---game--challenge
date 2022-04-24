d2464fcc14bb1a5708b7a9ea1cd92026b6701b40
Wan Lucas <wan.lucas.90@gmail.com>
2022-04-24 20:11:55 -0300

    fix

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 const rulesHTML = document.querySelector('.rules');
-const cBoxHTML = document.querySelector('.choise-box');
+const cBoxHTML = document.querySelector('.choice-box');
 const playingHTML = document.getElementById('playing');
 
-var pChoise, compCHoise, score = 0; 
+var pChoice, compCHoise, score = 0; 
 
 document.querySelector('#rules').addEventListener("click", ()=>{
-    rulesHTML.style.animation = 'actived 0.6s forwards';
+    rulesHTML.style.animation = 'active 0.6s forwards';
     document.body.style.setProperty('--op', 0.2);
 });
 document.getElementById('rules-close').addEventListener('click', ()=>{
@@ -13,15 +13,15 @@ document.getElementById('rules-close').addEventListener('click', ()=>{
     document.body.style.setProperty('--op', 1);
 });
 
-document.querySelectorAll('.choise-box button').forEach(bt=>{
+document.querySelectorAll('.choice-box button').forEach(bt=>{
     bt.addEventListener("click", ()=>{
         playingHTML.style.animation = '';
         setTimeout(()=>{
             cBoxHTML.style.visibility = "hidden";
             playingHTML.style.visibility = "visible";
             playingHTML.style.animation = 'moveRight 1s forwards';
-            updatePlayerChoise(bt.id);
-            updateCompChoise();
+            updatePlayerChoice(bt.id);
+            updateCompChoice();
             startAnimations();
         },300)
     });
@@ -34,35 +34,35 @@ document.getElementById('play-again').addEventListener("click",()=> {
     cBoxHTML.style.visibility = "visible";
 });
 
-function updatePlayerChoise(choise) {
-    playingHTML.querySelector('#player-choise').innerHTML = `
+function updatePlayerChoice(choice) {
+    playingHTML.querySelector('#player-choice').innerHTML = `
     <div>
-    <button class="choise-bt" id="${choise}" aria-label="${choise}"><img src="images/icon-${choise}.svg" alt="${choise}"></button>
+    <button class="choice-bt" id="${choice}" aria-label="${choice}"><img src="images/icon-${choise}.svg" alt="${choise}"></button>
     </div>`
-    pChoise = choise;
+    pChoice = choice;
 };
-function updateCompChoise() {
-    const choises = [ 'rock','paper','scissors'];
-    const choise = choises[Math.round(Math.random() * 2)];
-    document.querySelector('#comp-choise').innerHTML = `
+function updateCompChoice() {
+    const choices = [ 'rock','paper','scissors'];
+    const choice = choices[Math.round(Math.random() * 2)];
+    document.querySelector('#comp-choice').innerHTML = `
     <div>
-       <button class="choise-bt" id="${choise}" aria-label="${choise}"><img src="images/icon-${choise}.svg" alt="${choise}"></button>
+      <button class="choice-bt" id="${choice}" aria-label="${choice}"><img src="images/icon-${choise}.svg" alt="${choise}"></button>
     </div>` 
-    compCHoise = choise;
+    compCHoise = choice;
 }
 function verifyWinner() {
     let tabel = ['rock','paper','scissors'], res;
-    if(pChoise == compCHoise) return 0;
-    if(tabel.findIndex(e=>e==pChoise) == 0) tabel.push(tabel.shift());
-    res = tabel[tabel.findIndex(e=> e==pChoise) - 1] == compCHoise ? 1 : 2;
+    if(pChoice == compCHoise) return 0;
+    if(tabel.findIndex(e=>e==pChoice) == 0) tabel.push(tabel.shift());
+    res = tabel[tabel.findIndex(e=> e==pChoice) - 1] == compCHoise ? 1 : 2;
     score += res == 1 ? 1 : -1;
     return res;
 }
 function updateScore() { 
-     document.getElementById('score').innerText = score;
+    document.getElementById('score').innerText = score;
 }
 function startAnimations() {
-    const btSTL = document.querySelector('#comp-choise button').style, resSTL = document.querySelector('.result').style;
+    const btSTL = document.querySelector('#comp-choice button').style, resSTL = document.querySelector('.result').style;
     const resultHTML = document.getElementById('result'), result = verifyWinner();
     if(result == 0) resultHTML.innerText = 'DRAW'
     else resultHTML.innerText = result == 1  ? 'YOU WIN': "YOU LOSE";
